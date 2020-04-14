7337fa726f3adab865b197632d5427ecdc3600e9
amduraes <andremdpereira@gmail.com>
2020-04-14 17:48:37 +0100

    react-quest-8

--- src/App.js
+++ src/App.js
@@ -3,20 +3,11 @@ import './App.css';
 import axios from 'axios';
 import QuoteCard from './components/QuoteCard';
 
-// const initialQuote = {
-//   character: 'Frank Grimes',
-//   quote:
-//     'I live in a single room above a bowling alley...and below another bowling alley.',
-//   image:
-//     'https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FFrankGrimes.png?1497567511887',
-// };
-
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      // card: intitialQuote,
-      card: [],
+      card: null,
     };
   }
 
@@ -40,7 +31,14 @@ class App extends React.Component {
     return (
       <>
         <div className='App'>
-          <QuoteCard card={this.state.card} />
+          {this.state.card ? (
+            <QuoteCard card={this.state.card} />
+          ) : (
+            <h1>
+              {' '}
+              <b>Nope, nothing.</b>{' '}
+            </h1>
+          )}
         </div>
         <button type='button' onClick={this.getQuote}>
           New Quote
